fd6b90c7d4146bddaa59328d6bfb2864332d54fb
Aleks_K <korobov_a_u@mail.ru>
2022-12-14 18:21:53 +0500

    1232

--- Program.cs
+++ Program.cs
@@ -2,3 +2,9 @@
 3 символа. Первоначальный массви можно внести с клавиатуры, либо задат на старте выполнения алгоритма.*/
 
 
+string[] array1 = new string[5] {"123", "23", "hello", "world", "res"};
+string[] array2 = new string[array1.Length];
+FillSecondArray(array1, array2);
+PrintArray(array2);
+
+
