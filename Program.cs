b30c09930f295dadd501a8e6edaf474abaa29fa8
Aleks_K <korobov_a_u@mail.ru>
2022-12-14 18:35:57 +0500

    Blok shema

--- Program.cs
+++ Program.cs
@@ -7,4 +7,21 @@ string[] array2 = new string[array1.Length];
 FillSecondArray(array1, array2);
 PrintArray(array2);
 
-
+void FillSecondArray(string[] array1, string[] array2)
+{
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[i] = array1[i];
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
